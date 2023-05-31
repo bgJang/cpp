ab06a1842a506a21c2c59231382676cad901f5e2
bogyeongjang <bogyeongjang@koreanair.com>
2023-05-31 09:01:09 +0900

    g++ > cmake build로 환경 변경중

---
+++ 명품_C++_프로그래밍/Chap03/10_연습문제/3-1_cmake/Tower.h
@@ -0,0 +1,10 @@
+class Tower 
+{
+private:
+    int height = 1;
+
+public:
+    Tower();
+    Tower(int height);
+    int getHeight();
+};
---
+++ 명품_C++_프로그래밍/Chap03/10_연습문제/3_1/Tower.hpp
@@ -0,0 +1,10 @@
+class Tower 
+{
+private:
+    int height;
+
+public:
+    Tower();
+    Tower(int height);
+    int getHeight();
+};
