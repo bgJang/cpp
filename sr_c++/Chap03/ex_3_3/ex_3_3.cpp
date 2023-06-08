7ddcdb448387d7d5a55412684c2bea5fa8f4fd35
bogyeongjang <bogyeongjang@koreanair.com>
2023-06-08 09:18:02 +0900

    예제 3-3

---
+++ sr_c++/Chap03/ex_3_3/ex_3_3.cpp
@@ -0,0 +1,36 @@
+// 명품 C++ 프로그래밍
+// 3.4 (p 110)
+// 예제 3-4
+#include <iostream>
+
+class Circle {
+public:
+	int radius;
+	Circle(); // 기본 생성자
+	Circle(int r); // 매개 변수 있는 생성자
+	double getArea();
+}; 
+
+Circle::Circle() {
+	radius = 1;
+	std::cout << "반지름 " << radius << " 원 생성" << std::endl;
+}
+
+Circle::Circle(int r) {
+	radius = r;
+	std::cout << "반지름 " << radius << " 원 생성" << std::endl;
+}
+
+double Circle::getArea() {
+	return 3.14*radius*radius;
+}
+
+int main() {
+	Circle donut; // 매개 변수 없는 생성자 호출
+	double area = donut.getArea();
+	std::cout << "donut 면적은 " << area << std::endl;
+
+	Circle pizza(30); // 매개 변수 있는 생성자 호출
+	area = pizza.getArea();
+	std::cout << "pizza 면적은 " << area << std::endl;
+}
